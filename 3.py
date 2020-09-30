18625dd641d69a14cfbe229eb7ad098348ec29c8
tokyo-berlin-git <72129646+tokyo-berlin-git@users.noreply.github.com>
2020-09-30 21:22:57 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,3 +4,10 @@ for i in range(n):
  erno, city = input('enter enrolment number and city ')
  D2.update({erno: city})
 print(D2)
+
+D={}
+for i in range(10):
+ num=int(input('enter number '))
+ D.update({num:num*num})
+print(D)
+
