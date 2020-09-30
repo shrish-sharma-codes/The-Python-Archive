c144fa6aada871d0b5ba527ee361288aae0be9a5
Shrish Sharma <shrish0608@gmail.com>
2020-09-30 21:15:43 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,3 +4,12 @@ for i in range(n):
  erno, city = input('enter enrolment number and city ')
  D2.update({erno: city})
 print(D2)
+
+D = {}
+n = int(input('enter no of students '))
+for i in range(n):
+ erno, name, email = input('enter enrolment number ,name, email')
+ D.update({erno: (name, email)})
+en_no = input('enter the enrolment number ')
+print(D.get(en_no))
+
