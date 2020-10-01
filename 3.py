009dea639975452b05916981b4e7b008ee465ad7
Kamesh Parashar <56021579+xKameshx@users.noreply.github.com>
2020-10-01 16:01:20 +0530

    ok

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
 D2 = {}
 n = int(input('Enter no of students '))
 for i in range(n):
- erno, city = input('enter enrolment number and city ')
+ erno, city = input('Enter Enrolment number and city ')
  D2.update({erno: city})
 print(D2)
