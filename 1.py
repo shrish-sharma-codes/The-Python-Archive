fe7e534624f4b54cd6655dbcc72b6892ba1db795
Kamesh Parashar <56021579+xKameshx@users.noreply.github.com>
2020-10-01 16:00:16 +0530

    new

--- 1.py
+++ 1.py
@@ -1,2 +1,2 @@
-week = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7:
+Week = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7:
 'Sunday'}
