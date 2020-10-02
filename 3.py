1d160a49d1136196f084193fc130721d3828e3f0
Lokesh Pandey <55993386+Lokithegreat@users.noreply.github.com>
2020-10-02 19:01:37 +0530

    Update 3.py
    beti

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
 D2 = {}
 n = int(input('Enter no of students '))
 for i in range(n):
- erno, city = input('enter enrolment number and city ')
+ erno, city = input('enter enrolment number and city Atre betichod')
  D2.update({erno: city})
 print(D2)
