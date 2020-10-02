6f21565b80e44cfa5f14d5c291591975c6593eb1
Lokesh Pandey <55993386+Lokithegreat@users.noreply.github.com>
2020-10-02 18:59:11 +0530

    Update 6.py
    Atre

--- 6.py
+++ 6.py
@@ -1,6 +1,6 @@
 D5 = {}
 n = int(input('enter no of students '))
 for i in range(n):
- erno, phy, chem, maths = input('enter enrolment number and marks in phy chem and maths')
+ erno, phy, chem, maths = input('enter enrolment number and marks in phy chem and maths ATRE MADARCHOD')
  D5.update({erno: (int(phy), int(chem), int(maths))})
 print(D5)
