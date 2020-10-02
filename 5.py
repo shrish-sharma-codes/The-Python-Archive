2b1d5103c143fdfe1e529c8cdc6507ae3c3eb8fc
Lokesh Pandey <55993386+Lokithegreat@users.noreply.github.com>
2020-10-02 18:54:52 +0530

    Update 5.py
    spelling sahi kro

--- 5.py
+++ 5.py
@@ -1,6 +1,6 @@
 D4 = {}
 n = int(input('enter no of students '))
 for i in range(n):
- erno, state = input('enter enrolment number and state ').split()
+ erno, state = input('enter Enrollment number and state ').split()
  D4.update({erno: state})
 print(D4)
