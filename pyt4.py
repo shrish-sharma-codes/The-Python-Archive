97b302e4f59a32405cbd963e02e1d428b4d73a99
Spandan Saxena <spandansaxena@gmail.com>
2021-10-01 00:11:31 +0530

    Fixed Code

--- pyt4.py
+++ pyt4.py
@@ -1,7 +1,7 @@
-def area_cyclinder( h, d):
-	area = 22/7*(d/2)*(d/2) + 2*22/7*(d/2)*h
-	print(“Area of cylinder = {}”.format(area))
-
-d  = float(input(“Enter the diameter of cylinder”))
-h = float(input(“Enter the height of cylinder”))
-area_cyclinder(h,d)
+pi=22/7
+height = float(input('Height of cylinder: '))
+radian = float(input('Radius of cylinder: '))
+volume = pi * radian * radian * height
+sur_area = ((2*pi*radian) * height) + ((pi*radian**2)*2)
+print("Volume is: ", volume)
+print("Surface Area is: ", sur_area)
