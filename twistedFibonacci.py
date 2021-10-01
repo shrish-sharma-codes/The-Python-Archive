04ce2cd70938eab1fc5fc9951af0affa089af452
Abhishek Gupta <90904360+ABHIGPT401@users.noreply.github.com>
2021-10-01 12:37:58 +0530

    twistedFibonacci.py
    #This program contains a new way that merges both the concepts of prime number and Fibonacci numbers

---
+++ twistedFibonacci.py
@@ -0,0 +1,29 @@
+#This program contains a new way that merges both the concepts of prime number and fibonacci numbers
+
+
+
+
+
+
+
+def isprime(num):
+    for i in range(2,num):
+       if (num % i) == 0:
+           return False
+       else:
+           return True
+n = int(input())
+a=0
+b=1
+list = []
+for i in range(0,n):
+    c=a+b
+    list.append(c)
+    a=b
+    b=c
+print(list)
+for index, elem in enumerate(list):
+    if isprime(elem) == True:
+        list[index]=0
+print(list)
+        
