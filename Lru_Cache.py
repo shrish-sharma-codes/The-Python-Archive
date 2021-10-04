9fd5f9461b7bcf1d4a115ed74162439629a32aa3
Nishkarsh Maitry <47109963+NicheApp@users.noreply.github.com>
2021-10-04 09:08:34 +0530

    Lru cache implementation using python

---
+++ Lru_Cache.py
@@ -0,0 +1,45 @@
+from collections import OrderedDict
+ 
+class LRUCache:
+ 
+    # initialising capacity
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+ 
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
+ 
+ 
+cache = LRUCache(2)
+ 
+ 
+cache.put(1, 1)
+print(cache.cache)
+cache.put(2, 2)
+print(cache.cache)
+cache.get(1)
+print(cache.cache)
+cache.put(3, 3)
+print(cache.cache)
+cache.get(2)
+print(cache.cache)
+cache.put(4, 4)
+print(cache.cache)
+cache.get(1)
+print(cache.cache)
+cache.get(3)
+print(cache.cache)
+cache.get(4)
+print(cache.cache)
+
+#This code is contributed by Nishkarsh Maitry
