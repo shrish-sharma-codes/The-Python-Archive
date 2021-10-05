1fd96e02186f371974690fab8f290bbbdc0187e5
Kamesh Parashar <56021579+xKameshx@users.noreply.github.com>
2021-10-05 16:02:50 +0530

    Create pdSeries.py

---
+++ pdSeries.py
@@ -0,0 +1,11 @@
+# import pandas as pd
+import pandas as pd
+ 
+# import numpy as np
+import numpy as np
+ 
+# simple array
+data = np.array(['g','e','e','k','s'])
+ 
+ser = pd.Series(data)
+print(ser)
